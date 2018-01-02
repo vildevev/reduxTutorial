a90012bd512d12b55522ca4676957a8a516c3921
Vilde Vevatne <vvevatne@pinterest.com>
2018-01-02 11:08:18 -0800

    Add state and action definitions

---
+++ redux.js
@@ -0,0 +1,7 @@
+// Everything that changes is contained in a single object: The state tree
+
+// State is read only. You need to dispatch an action if you want to change the state. 
+
+// The state = a minimal representation of the data in your app
+
+// The action = the minimal representation of the change to that data
